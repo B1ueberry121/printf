75c374f2beb4c13f0944f9673d8ccad14806dfad
B1ueberry121 <2710@holbertonschool.com>
2021-03-17 18:38:57 +0000

    adding puts function and other new modulos

--- holberton.h
+++ holberton.h
@@ -8,11 +8,13 @@
 #define SPECIAL_CHAR '%'
 
 int _printf(const char *format, ...);
+int _puts(char *str);
 int _putchar(char c);
 int _strlen(char *s);
 int _printstring(va_list vl);
 int _printchar(va_list vl);
 int _putstring(char *s);
 int _printDec(va_list arg);
+int convert(unsigned int num, int base, int flag);
 
 #endif
